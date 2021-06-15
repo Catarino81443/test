d572775b3a57713e179d35309a68d3e081d56368
anama <aluno81443@ipt.pt>
2021-06-15 14:39:06 +0100

    Initiall Commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+import os
+
+from setuptools import setup, find_packages
+setup(
+    name="namemodules",
+    version="0.1",
+    description="Name Distribution Package",
+    packages=find_packages(),
+    include_package_data=True
+)
